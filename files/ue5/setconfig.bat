6fdff8a00fa9d9c88d051e2b9d8de48c0fe8622f
Marc Aubanel <maubanel@mac.com>
2024-10-05 07:34:44 -0500

    Update setconfig.bat

--- files/ue5/setconfig.bat
+++ files/ue5/setconfig.bat
@@ -2,23 +2,24 @@
 
 chcp 65001
 
-echo P4IGNORE=.p4ignore > .p4config
-echo P4PORT=ssl:enterperforcerepo:6666 >> .p4config
+set /p server="Enter your Perforce server location (for example, ssl:servername.com:1666)? "
+echo P4PORT=%server% >> %~dp0\.p4config
+
+echo P4IGNORE=.p4ignore > %~dp0\.p4config
 
 set /p username=What is your P4V Username? 
 echo P4USER=%username% >> .p4config
 
-set /p host=What is your P4V Host (Look in Advanced in Edit Workspace)?
-echo P4HOST=%host% >> .p4config
+echo P4HOST=%%COMPUTERNAME%% >> %~dp0\.p4config
 
 set /p workspace=What is your P4V Workspace (Look in Advanced in Edit Workspace) ? 
-echo P4CLIENT=%workspace% >> .p4config
+echo P4CLIENT=%workspace% >> %~dp0\.p4config
 
 echo Press Return to set up P4CONFIG
 echo.
 pause
 
-p4 set P4CONFIG=.p4config
+p4 set P4CONFIG=%~dp0\.p4config
 
 echo Press Return to print output of p4 set
 echo.
